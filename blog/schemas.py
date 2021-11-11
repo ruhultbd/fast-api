88866bc526ec65ee692be16034bf3d4443600926
ruhultbd <ruhulamin@technobd.com>
2021-11-11 15:01:23 +0600

    blog crud with first API

--- blog/schemas.py
+++ blog/schemas.py
@@ -5,3 +5,11 @@ class Blog(BaseModel):
     title: str
     body: str
     is_active: Optional[bool]=True
+
+class ShowBlog(BaseModel):
+    title: str
+    body: str
+
+    class Config():
+        orm_mode=True
+
